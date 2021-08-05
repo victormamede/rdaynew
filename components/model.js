a6b1407e98b1fde51386d6d786e4cc9b1c24643c
Victor Mamede <victoraugustomamede@hotmail.com>
2021-08-05 19:47:31 -0300

    added spinner

--- components/model.js
+++ components/model.js
@@ -55,18 +55,40 @@ export default function Model({ className }) {
   }, []);
 
   return (
-    <Canvas className={className}>
-      <PerspectiveCamera
-        makeDefault
-        rotation={transform.rotation.toArray()}
-        position={transform.position.toArray()}
-      />
-      <Suspense fallback={null}>
+    <Suspense
+      fallback={
+        <div className='w-screen h-screen flex items-center justify-center'>
+          <div className='h-20 w-20'>
+            <svg
+              aria-hidden='true'
+              focusable='false'
+              data-prefix='fas'
+              data-icon='spinner'
+              className='animate-spin'
+              role='img'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 512 512'
+            >
+              <path
+                fill='currentColor'
+                d='M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z'
+              ></path>
+            </svg>
+          </div>
+        </div>
+      }
+    >
+      <Canvas className={className}>
+        <PerspectiveCamera
+          makeDefault
+          rotation={transform.rotation.toArray()}
+          position={transform.position.toArray()}
+        />
         <group position={[0, -1, 0]}>
           <Monke />
           <Environment preset='forest' />
         </group>
-      </Suspense>
-    </Canvas>
+      </Canvas>
+    </Suspense>
   );
 }
